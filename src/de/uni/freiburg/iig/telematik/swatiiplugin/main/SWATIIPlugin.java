0745454f32bf1f07539c2495f2d6604573813f9a
Simon <mrmcx@hotmail.de>
2015-10-17 14:51:48 +0200

    1

--- src/de/uni/freiburg/iig/telematik/swatiiplugin/main/SWATIIPlugin.java
+++ src/de/uni/freiburg/iig/telematik/swatiiplugin/main/SWATIIPlugin.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package swatiiplugin;
+package de.uni.freiburg.iig.telematik.swatiiplugin.main;
 
 /**
  *
